2bcc719b6b1ebf24b35e67c46908226a1b263c43
kaamaDE <rene.szczepanski@pm.me>
2025-09-01 08:24:34 +0200

    Implement initial project structure and setup

---
+++ common/src/main/java/de/kaama/ExampleMod.java
@@ -0,0 +1,9 @@
+package de.kaama;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "mobcapture";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/de/kaama/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package de.kaama.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import de.kaama.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
